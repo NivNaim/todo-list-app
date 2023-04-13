2e77fbfddc7aaa3e8dbd9de636f5b091a15adafa
Niv Naim <nivnaimwork@gmail.com>
2023-04-13 03:33:12 +0300

    style: remove home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodayTodosComponent } from './today-todos/today-todos.component';
 import { UpcomingTodosComponent } from './upcoming-todos/upcoming-todos.component';
 import { CompletedTodosComponent } from './completed-todos/completed-todos.component';
 import { AppComponent } from './app.component';
-
+import { TodayTodosComponent } from './today-todos/today-todos.component';
 
 const appRoutes: Routes = [
   { path: '', component: AppComponent, pathMatch: 'full' },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  // },
   { path: 'today-todos', component: TodayTodosComponent },
   { path: 'upcoming-todos', component: UpcomingTodosComponent },
   { path: 'completed-todos', component: CompletedTodosComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
 import { InboxTodosComponent } from './inbox-todos/inbox-todos.component';
-import { TodayTodosComponent } from './today-todos/today-todos.component';
 import { UpcomingTodosComponent } from './upcoming-todos/upcoming-todos.component';
 import { CompletedTodosComponent } from './completed-todos/completed-todos.component';
+import { TodayTodosComponent } from './today-todos/today-todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent,
     SearchBarComponent,
     ToolBarComponent,
     InboxTodosComponent,
