29f47629382fc2258853993ae28c9e569b8a4feb
Niv Naim <nivnaimwork@gmail.com>
2023-04-14 19:08:33 +0300

    feat: create reactive form logic for addTask form

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -3,7 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-  tasks: [{ id: string; title: string; date: Date }];
+  tasks: { id: string; title: string; date: string }[] = [];
 
-  addTask() {}
+  addTask(title: string, date: string) {
+    const taskId = uuidv4();
+    const task = {
+      id: taskId,
+      title: title,
+      date: new Date(date).toISOString(),
+    };
+    this.tasks.push(task);
+  }
 }
