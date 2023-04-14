f2f91447176bcea19e7a88d10ece062b63990c55
Niv Naim <nivnaimwork@gmail.com>
2023-04-14 18:03:37 +0300

    style: change style for add task button in add-task-form

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.scss']
+  styleUrls: ['./add-task.component.scss'],
 })
-export class AddTaskComponent {
-
-}
+export class AddTaskComponent {}
