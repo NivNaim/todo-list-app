f8ddbc20290437d9250b9c016838e1615fa3bcf8
Niv Naim <nivnaimwork@gmail.com>
2023-04-15 20:23:07 +0300

    feat: display tasks in inbox-tasks component

--- src/app/inbox-tasks/inbox-tasks.component.ts
+++ src/app/inbox-tasks/inbox-tasks.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-inbox-tasks',
   templateUrl: './inbox-tasks.component.html',
   styleUrls: ['./inbox-tasks.component.scss'],
 })
-export class InboxTasksComponent {}
+export class InboxTasksComponent implements OnInit {
+  tasks: { id: string; title: string; date: string }[];
+
+  constructor(private tasksService: TasksService) {}
+
+  ngOnInit() {
+    this.tasks = this.tasksService.tasks;
+  }
+}
