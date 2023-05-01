b47561b41c12285ca8df08f1881ac0cc7974dee2
netanel <natanelhenya@gmail.com>
2023-05-01 20:57:27 +0300

    feat: changed dark mode to use css variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
